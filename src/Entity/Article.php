42edcad43236957034b5c0ee099c9266479a4aaa
Thomas Chevron <chevron.thomas33@gmail.com>
2022-07-06 10:40:14 +0200

    create DB Doctrine COMMENTARY

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,16 +4,24 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+// Création d'un Objet Relationnel de Mapping (ORM) avec le frameworl Doctrine
+
 /**
  * @ORM\Entity()
  */
+
+// Création d'une classe ici nommée "Article" qui sera le nom de la BDD
 class Article {
+
+    //Je donne les valeurs des colonnes de ma classe
 
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id()
      * @ORM\GeneratedValue()
      */
+
+    // Je transforme le titre de la colonne en méthode
     public $id;
 
     /**
