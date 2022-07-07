daf3beac2701e66209e6d18279ae63a3e6b78fa1
Thomas Chevron <chevron.thomas33@gmail.com>
2022-07-07 16:12:55 +0200

    New Category in Table

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,8 +25,30 @@ class CategoryController extends AbstractController
 
         $entityManager->persist($category);
         $entityManager->flush();
+    }
 
-        dd($category);
+    /**
+     * @Route("/categories", name="list_categories")
+     */
+
+    public function listCategories(CategoryRepository $categoryRepository){
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('list_categories.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+
+
+    /**
+     * @Route("/categories/{id}", name="show_category")
+     */
+    public function showCategory($id, CategoryRepository $categoryRepository){
+        $category = $categoryRepository->find($id);
+
+        return $this->render('show_category.html.twig', [
+            'category' => $category
+        ]);
 
     }
 }
